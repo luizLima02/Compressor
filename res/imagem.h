fcea70da78bc1e6f9bb37e498e4789167ec48e42
Luiz <luizlimanetto@gmail.com>
2023-11-26 16:12:38 -0300

    corrigido leitura do benchmark

--- res/imagem.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#ifndef IMAGE
-#define IMAGE
-
-
-
-
-#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
+#include<stdlib.h>
+#include"imagem.h"
+#include<string>
+
+
+std::string caminhoSalvo = "ImagensSalvas";
+
 
 int main(){
+	system("cls");
 	std::cout << "start conversor!\n";
+
+	std::string nomeArquivo = "copia2.bmp";
+
+	Imagem img(640, 480);
+	img.ler("benchmark.bmp", 1,0,2);
+
+	std::string saida = caminhoSalvo + "/" + nomeArquivo;
+
+	img.salvar(saida.c_str());
+
+	system("pause");
 	return 0;
 }
